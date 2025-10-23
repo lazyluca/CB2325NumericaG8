06fca26ead2be27d68e6e005849a98f0d9199926
AFelipeL <al.felipe.oliveira@impatech.edu.br>
2025-10-23 13:30:49 -0300

    Representação Gráfica
    Added a function to plot the integral area using matplotlib.

--- integracao.py
+++ integracao.py
@@ -1,4 +1,8 @@
 import numpy as np
+import matplotlib.pyplot as plt
+
+a = 0
+b = np.pi
 
 def integral(funcao, a, b, n=100):
     '''
@@ -20,3 +24,37 @@ def integral(funcao, a, b, n=100):
     valor_integral = (delta/2)*(y[0] + 2*np.sum(y[1:-1]) + y[-1])
 
     return float(f"{valor_integral:.4g}")
+
+def grafico(a , b , s, n=20):
+    ''' dados curva suave '''
+    s = 100
+    x_curva = np.linspace(a , b , s)
+    y_curva = np.sin(x_curva)
+
+    ''' número de trapézios '''
+    n = 20
+    '''vértices do trapézio '''
+    x_disc = np.linspace(a , b , n+1)
+    y_disc = np.sin(x_disc)
+
+
+    plt.plot(x_curva,y_curva, label = "f(x) = sen(x)", color = "blue", alpha = 0.8)
+    plt.fill_between(x_disc,y_disc,0, label = "Área da Integral", color = "skyblue", alpha = 0.8)
+    plt.vlines(x_disc,0,y_disc, color = "red" , linestyle = "--" , linewidth = 1)
+    plt.plot(x_disc,y_disc, color = "red" , linewidth = 1.5)
+
+    plt.title("Aproximação da Integral")
+    plt.xlabel("Eixo X")
+    plt.ylabel("Eixo Y")
+    plt.legend()
+    plt.grid(True, linestyle = ":" , alpha = 0.6)
+    
+
+    return plt.show()
+
+
+f = lambda x: np.sin(x)
+area = integral(f, 0, np.pi, n=100)
+print(area)
+s=100
+grafico(a , b , s , n=20)
