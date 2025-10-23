857fec01ecebaa6ad75b00ced99c8efde93350ca
ingrid-s-c <al.ingrid.carvalho@impatech.edu.br>
2025-10-22 22:29:41 -0300

    Estruturação inicial do módulo Raízes de Funções

--- raizes.py
+++ raizes.py
@@ -1,1 +1,16 @@
 '''implementação do método das raízes'''
+
+def raiz(funcao, a, b, tol, method):
+
+    if method == "bissecao":
+        pass
+
+    elif method == "secante":
+        pass
+
+    elif method == "newton_raphson":
+        pass
+
+    else:
+        pass
+        #Exibir mensagem de erro: "método não reconhecido"    
