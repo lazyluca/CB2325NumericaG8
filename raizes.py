a4d83bd3b512ea7ee000bf1a962c8f6270f4c323
dulce-costa <al.dulce.costa@impatech.org.br>
2025-10-24 16:18:54 -0300

    criacao metodo bissecao
    Implementa a lógica principal do método da Bisseção.
    O código está funcional, mas ainda falta documentação (docstrings)
    e o aprimoramento do código em si.

--- raizes.py
+++ raizes.py
@@ -12,23 +12,50 @@ def raiz(funcao, a, b, tol, method):
         return Newton(funcao,a,b,tol)
 
     else:
-        #Exibir mensagem de erro: "método não reconhecido"    
-        pass
-        
-
-    def Bissecao(funcao,a,b,tol):
-        '''parametros: função, intervalos e tolerância'''
-        '''retornar erro se na verificação de sinais der f(a).f(b)>0, ou seja, raiz não está entre esse intervalo, se der certo(<0), continua'''
-        '''estrutura principal: loop até encontrar a raiz (valor encontrado menor que a tolerância) ou o número de iterações ultrapassar o esperado(prevenir loop infinito se algo der errado)'''
-        ''' ideia: calcular o ponto médio do intervalo, analisar os sinais, modificar o intervalo para o ponto medio até a ou b e repetir...'''
-        ''' ou seja: Verificar Sinais(f(a).f(b)<0) -> Loop -> Calcular Meio -> Salvar Meio -> Decidir Nova Metade -> Repetir até encontrar uma raiz'''
-       
+        #Exibir mensagem de erro: "método não reconhecido"
         pass
 
-    def Secante(funcao,a,b,tol):
-        #Implementar o Método da Secante
-        pass
 
-    def Newton(funcao,a,b,tol):
-        #Implementar o Método de Newton-Raphson
-        pass
+def Bissecao(funcao,a,b,tol):
+    f_a=funcao(a)
+    f_b=funcao(b)
+    if f_a*f_b>0:
+        raise ValueError("Erro: f(a) e f(b) têm o mesmo sinal. O método não pode garantir uma raiz no intervalo.")
+    elif f_a==0:
+        return a,[a]
+    elif f_b==0:
+        return b,[b]
+    else:
+        iter=0
+        iter_para_plote=[]
+        while (b-a)/2>tol and iter<1000:
+            m=(a+b)/2
+            f_m=funcao(m)
+            iter_para_plote.append(m)
+            iter+=1
+            if f_m==0:
+                break
+            if f_a*f_m<0:
+                b=m
+                f_b=f_m
+            else:
+                a=m
+                f_a=f_m
+    return (a+b)/2,iter_para_plote
+
+def Secante(funcao,a,b,tol):
+    #Implementar o Método da Secante
+    pass
+
+def Newton(funcao,a,b,tol):
+    #Implementar o Método de Newton-Raphson
+    pass
+
+if __name__=="__main__":
+  f=lambda x: x**2-2
+  valor1,_=raiz(f,0,2,1e-6,"bissecao")
+  print(valor1)
+
+  fi= lambda x: x**2-4
+  valor2,_=raiz(fi,0,4,1e-6,"bissecao")
+  print(valor2)
