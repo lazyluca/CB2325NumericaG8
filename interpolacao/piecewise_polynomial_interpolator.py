3ea0248915d69e4fd4f09ce3f63bdfce4a51d4bf
Rinoceda <al.tiago.ribeiro@impatech.edu.br>
2025-10-28 07:52:23 -0300

    Update piecewise_polynomial_interpolator.py
    Início do interpolador

--- interpolacao/piecewise_polynomial_interpolator.py
+++ interpolacao/piecewise_polynomial_interpolator.py
@@ -1,1 +1,18 @@
-#teste
+import numpy as np
+import matplotlib.plotly as plt
+
+class Interpolador_Linear_por_Partes()
+
+  def __init__(self, lista_x: list, lista_y:list):
+
+    self.x = lista_x
+    self.y = lista_y
+
+  def interpolar(x):
+
+    if x<self.x[0] or x>self.x[-1]:
+      return ValueError('Erro de Extrapolação: a abscissa a ser avaliada está fora do intrevalo de interpolação.
+    else:
+      
+                        
+    
