c3262f29fa9cb4e41870bcaa9f2842bb6bc9fa00
ingrid-s-c <al.ingrid.carvalho@impatech.edu.br>
2025-10-27 22:38:14 -0300

    Adição de novos parâmetros em raizes. Segunda versão do Método de Newton-Raphson.

--- raizes/raizes.py
+++ raizes/raizes.py
@@ -1,20 +1,20 @@
-'''implementação do método das raízes'''
+#Implementação do método das raízes
 
-def raiz(funcao, a, b, tol, method):
+def raiz(funcao, a=None, b=None, tol=None, f_prime=None, max_iter=1000, method=None):
 
     if method == "bissecao":
-        return bissecao(funcao,a,b,tol)
+        return bissecao(funcao, a, b, tol,max_iter)
 
     elif method == "secante":
-        return secante(funcao,a,b,tol)
+        return secante(funcao,a,b,tol,max_iter)
 
     elif method == "newton_raphson":
-        return newton(funcao,a,b,tol)
+        return newton(funcao,a,tol,f_prime, max_iter)
 
     else:
         raise ValueError("Método não reconhecido")
 
-def bissecao(funcao, a, b, tol):
+def bissecao(funcao, a, b, tol,max_iter):
 
     f_a = funcao(a)
     f_b = funcao(b)
@@ -52,10 +52,11 @@ def bissecao(funcao, a, b, tol):
 
     return (a+b)/2, iter_para_plot
 
-def secante(funcao,a,b,tol):
+def secante(funcao,a,b,tol,max_iter):
     iter=0
+    iter_para_plot=[]
 
-    while iter<10000:
+    while iter<max_iter:
 
         f_a=funcao(a)
         f_b=funcao(b)
@@ -65,26 +66,50 @@ def secante(funcao,a,b,tol):
         
         c=b-f_b*((b-a)/(f_b-f_a))
         f_c=funcao(c)
+        iter_para_plot.append(c)
 
         if(abs(c-b)<tol or abs(f_c)<tol):
-            return c,[c]
+            return c,iter_para_plot
         a=b
         b=c
         iter+=1
     raise RuntimeError("Número máximo de iterações atingido sem convergência.")
 
 
-def newton(funcao,a,b,tol):
-    #Implementar o Método de Newton-Raphson
-    pass
+def newton(funcao, a, tol, f_prime, max_iter):
+    iter=0
+    iter_para_plot=[]
 
+    while iter<max_iter:
+        f_a=funcao(a)
+
+        if(f_prime==None):
+            pass
+        else:
+            f_prime_a=f_prime(a)
+
+        if f_prime_a==0:
+            raise ZeroDivisionError("Erro: derivada zero f_prime(a) = 0. Divisão por zero.")
+        
+        c=a-f_a/f_prime_a
+        iter_para_plot.append(c)
+        
+        if(abs(c-a)<tol):
+            return c,iter_para_plot
+        
+        a=c
+        iter+=1
+    
+    raise RuntimeError("Número máximo de iterações atingido sem convergência.")
+
+#Testes temporários
 if __name__=="__main__":
   f=lambda x: x**2-2
-  valor1,_=raiz(f,0,2,1e-6,"bissecao")
+  valor1,_=raiz(f,0,2,1e-6,method="bissecao")
   print(valor1)
 
   fi= lambda x: x**2-4
-  valor2,_=raiz(fi,0,4,1e-6,"bissecao")
+  valor2,_=raiz(fi,0,4,1e-6,method="bissecao")
   print(valor2)
 
   f=lambda x:x**3-9*x+5
@@ -92,9 +117,10 @@ if __name__=="__main__":
   print(f"{raiz_0:.3f}")
 
   g= lambda x: x**10-5
-  valor2,_=raiz(g,0,4,1e-6,"bissecao")
+  valor2,_=raiz(g,0,4,1e-6,method="bissecao")
   print(valor2)
 
   h = lambda x: x**10-5
-  valor2,_=raiz(h,0,4,1e-6,"bissecao")
+  h_prime = lambda x: 10*x**9
+  valor2=raiz(h,2,tol=1e-6,f_prime=h_prime, method="newton_raphson")
   print(valor2)
