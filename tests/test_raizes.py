e9622aadb55d703212a4679db0206a8745b68420
ingrid-s-c <al.ingrid.carvalho@impatech.edu.br>
2025-10-30 11:01:41 -0300

    Correção: o commit anterior foi feito em local errado, ele se refere ao test_raizes.py: Importação do pytest e implementação inicial de testes de ValueError

--- tests/test_raizes.py
+++ tests/test_raizes.py
@@ -1,1 +1,31 @@
 #desenvolvimento de testes para o metodo das raizes. 
+
+import pytest
+import math
+from raizes.raizes import raiz 
+
+#Exemplos de funções
+def f1(x):
+    return x**2 - 4
+
+def f2(x):
+    return math.exp(x) - 2
+
+def f3(x):
+    return x**3 - x - 2
+
+
+def test_metodo_invalido():
+    """
+    Testa se a função levanta ValueError para método inválido.
+    """
+    with pytest.raises(ValueError, match="Método não reconhecido"):
+        raiz(f1, a=0, b=3, method="nao_existe")
+
+def test_bissecao_intervalo_invalido():
+    """
+    Testa se o método levanta ValueError quando o intervalo não contém raiz.
+    """
+    with pytest.raises(ValueError, match="mesmo sinal"):
+        raiz(f1, a=3, b=4, tol=1e-6, method="bissecao")
+
