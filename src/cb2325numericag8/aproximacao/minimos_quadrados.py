d9c88ea6c318e7c8db4c0555053478df18735231
heitor-brazuna <al.heitor.brazuna@impatech.edu.br>
2025-11-09 21:57:15 -0300

    Renomeando a função e adicionando mais raises
    Adição de 4 novos raises, renomeação da função para AproximacaoPolinomial e retirando o teste unitário

--- src/cb2325numericag8/aproximacao/minimos_quadrados.py
+++ src/cb2325numericag8/aproximacao/minimos_quadrados.py
@@ -2,19 +2,22 @@ import numpy as np
 from cb2325numericag8.grafico.grafico_minimos_quadrados import plot_aproximacao
 from cb2325numericag8.aproximacao.regressao_linear import ajuste_linear
 
-def aproximacao_polinomial(abscissas : list, ordenadas : list, grau : int = 1, plot: bool = False, n: int = 500):
+def AproximacaoPolinomial(abscissas: list, ordenadas: list, grau: int = 1, plot: bool = False, n: int = 500) -> list:
     """
     Aproxima os pontos (x, y) por um polinômio de grau n usando mínimos quadrados e plota o gráfico.
 
     Parâmetros:
-        abscissas (list ou array): coordenadas x
-        ordenadas (list ou array): coordenadas y
+        abscissas (list ou tuple): coordenadas x
+        ordenadas (list ou tuple): coordenadas y
         grau (int, opicional): grau do polinômio. Padrão é 1
-        plot (bool, opicional): se for True plota o gráfico. Padrão é False
-        n_suave (int, opcional): Número de pontos para a curva suave caso plot = True . Padrão é 500
+        plot (bool, opicional): Se True, plota o gráfico. Padrão é False
+        n (int, opcional): Número de pontos para a curva suave caso plot = True . Padrão é 500
     
     Raises:
-        ValueError: Se o grau fornecido não for um número inteiro
+        TypeError: Se as abscissas não forem do tipo 'list' ou 'tuple'
+        TypeError: Se as ordenadas não forem do tipo 'list' ou 'tuple'
+        TypeError: Se o grau fornecido não for um 'int' ou um 'float' com parte decimal igual a 0
+        ValueError: Se as abscissas ou as ordenadas form listas vazias
         ValueError: Se o grau fornecido for menor que 1
         ValueError: Se as abicissas e as ordenadas tiverem um número diferente de elementos
         ValueError: Se o grau fornecido for maior ou igual à quantidade de abcissas e ordenadas
@@ -23,28 +26,49 @@ def aproximacao_polinomial(abscissas : list, ordenadas : list, grau : int = 1, p
         list: coeficientes do polinômio aproximado, do maior para o menor grau
     """
     
-    if not isinstance(grau, int):
+    if not isinstance(abscissas, (list, tuple)):
+        raise TypeError(
+            "O argumento 1 de AproximacaoPolinomial (abscissas) precisa ser 'list' ou 'tuple'"
+        )
+    
+    if not isinstance(ordenadas, (list, tuple)):
+        raise TypeError(
+            "O argumento 2 de AproximacaoPolinomial (ordenadas) precisa ser 'list' ou 'tuple'"
+        )
+            
+    if not isinstance(grau, (int,float)):
+        if grau != int(grau):
+            raise TypeError(
+                "O argumento 3 de AproximacaoPolinomial (grau do polinômio) precisa ser 'int'"
+            )
+    
+    if len(abscissas) == 0:
         raise ValueError(
-            "O grau do polinômio aproximado precisa ser um número inteiro"
+            "A quantidade de abscissas (argumento 1) precisa ser maior que 0"
+        )
+    
+    if len(ordenadas) == 0:
+        raise ValueError(
+            "A quantidade de ordenadas (argumento 2) precisa ser maior que 0"
         )
     
     if grau<1:
         raise ValueError(
-            "O grau do polinômio aproximado precisa ser maior ou igual a 1"
+            "O grau do polinômio aproximado (argumento 3) precisa ser maior ou igual a 1"
         )
     
     if len(abscissas) != len(ordenadas):
         if len(abscissas) < len(ordenadas):
             raise ValueError(
-                "O número de abscissas (x) é menor que o número de ordenadas (y)"
+                "O número de abscissas (argumento 1) é menor que o número de ordenadas (argumento 2)"
             )
         raise ValueError(
-            "O número de abscissas (x) é maior que o número de ordenadas (y)"
+            "O número de abscissas (argumento 1) é maior que o número de ordenadas (argumento 2)"
         )
 
     if len(abscissas) <= grau:
         raise ValueError(
-            "O grau do polinômio aproximado deve ser menor que o número de pontos fornecidos"
+            "O grau do polinômio aproximado (argumento 3) deve ser menor que o número de pontos fornecidos (argumento 1)"
         )
     
     if grau == 1:
@@ -68,16 +92,4 @@ def aproximacao_polinomial(abscissas : list, ordenadas : list, grau : int = 1, p
         plot_aproximacao(abscissas, ordenadas, coeficientes, n)
     
     return coeficientes
-
-if __name__ == "__main__":
-    """
-    Bloco de teste para executar o código.
-    """
-    x = [-2, -1, 0, 1, 2]
-    y = [-2, 1, 3, 5, 2]
-    grau = 3  
-    n = 500
-    
-    coeficientes = aproximacao_polinomial(x, y, grau, True)
-    print([float(f"{coef:.2f}") for coef in coeficientes])
 
